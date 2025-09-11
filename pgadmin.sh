7ad950a2d0d52fa7ead6770ed1fb2762ce7f1a46
Dzx <zekeding@outlook.com>
2025-09-11 12:56:28 +0800

    feat(docker): add pgadmin volume

--- pgadmin.sh
+++ pgadmin.sh
@@ -2,6 +2,7 @@
 docker run --name pgadmin -p 8080:80 \
     -e "PGADMIN_DEFAULT_EMAIL=dzx@dzx.com" \
     -e "PGADMIN_DEFAULT_PASSWORD=dzx123" \
+    -v pgadmin_data:/var/lib/pgadmin \
     -d dpage/pgadmin4
 # Start SSH tunnel
 ssh -L 5433:localhost:5432 ubuntu@3.26.244.193 -N
